0b97f782052e1474a828d73dbcb8b7da5e9b3ae6
CarlosPiant <ib_pineda@hotmail.com>
2025-12-01 00:03:01 -0800

    Update test_one_shot_single_point.py

--- tests/test_one_shot_single_point.py
+++ tests/test_one_shot_single_point.py
@@ -3,6 +3,12 @@ import pandas as pd
 import neuroconnect_app as appmod  # hypothetical module name
 
 def test_one_shot_single_point_has_no_edges():
+    """
+    author: Carlos Pineda
+    reviewer: Tina
+    category: one shot test
+    description: Test that a single point does not produce edges
+    """
     df = pd.DataFrame([{"x": 0.0, "y": 0.0, "z": 0.0, "id": "only"}])
     # Distance builders should yield no edges
     e_knn = appmod.build_edges_knn(df, k=4)
